9a644f1c13270502ec9f2f8d7994ff05660e8eb1
Mehified <wineandbread1692@gmail.com>
2022-06-23 21:41:54 -0400

    moved mine for testing

--- quest/quest.py
+++ quest/quest.py
@@ -37,7 +37,21 @@ class Quest(commands.Cog):
         """Mining action"""
         #check stamina
         await ctx.send("Mining...")
-        await Mining.mine(ctx, var)
+        oreList = ["stone", "coal", "copper", "iron", "silver", "gold", "platinum", "titanium", "diamond"]
+        oreProb = [0.9, 0.7, 0.55, 0.5, 0.4, 0.25, 0.15, 0.1, 0.05, 0.005]
+        mineResult = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        mineAttempt = 0
+        embed = discord.Embed(colour=0xFF00FF, description="__Result__")
+        embed.set_thumbnail(url='https://i.imgur.com/UioE5ls.png')
+        embed.add_field(name = "stone", value = "1")
+        '''
+        for ore in oreList:
+            mineAttempt = random()
+            if mineAttempt < oreProb[ore]:
+                mineResult[ore] += 1
+                embed.add_field(name = oreList[ore], value = "1")
+        '''
+        await ctx.send(embed=embed)
 
     @commands.command()
     async def queststart(self, ctx):
