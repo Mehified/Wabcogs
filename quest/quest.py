9001005410bb4baabd5676c3068f7a1be88abe09
Mehified <wineandbread1692@gmail.com>
2022-06-23 21:43:24 -0400

    loop test

--- quest/quest.py
+++ quest/quest.py
@@ -41,16 +41,14 @@ class Quest(commands.Cog):
         oreProb = [0.9, 0.7, 0.55, 0.5, 0.4, 0.25, 0.15, 0.1, 0.05, 0.005]
         mineResult = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         mineAttempt = 0
-        embed = discord.Embed(colour=0xFF00FF, description="__Result__")
-        embed.set_thumbnail(url='https://i.imgur.com/UioE5ls.png')
-        embed.add_field(name = "stone", value = "1")
-        '''
+        embed = discord.Embed(colour=0xFF00FF, description="__**Result**__")
+        
         for ore in oreList:
             mineAttempt = random()
             if mineAttempt < oreProb[ore]:
                 mineResult[ore] += 1
                 embed.add_field(name = oreList[ore], value = "1")
-        '''
+
         await ctx.send(embed=embed)
 
     @commands.command()
